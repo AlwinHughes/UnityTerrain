f5dd67dd5d0e3071b03575bf9ad6cbdf539059cc
alwinhughes <alwin.hughes@btopenworld.com>
2020-03-04 02:34:25 +0000

    should have been in last commit

---
+++ Assets/FlatTerain/ChunkGroupOpt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkGroupOpt : ScriptableObject {
+
+  [Range(0,10)]
+  public int height;
+  [Range(0,10)]
+  public int width;
+
+  [Range(2,200)]
+  public int res;
+
+  public ChunkGroupOpt(int height, int width, int res) {
+    this.height = height;
+    this.width = width;
+    this.res = res;
+  }
+
+}
